48b4507b4c03b27299c66bdf598e60e21c2923eb
retke <laggron42@gmail.com>
2018-05-05 21:47:10 +0200

    [V3 RoleInvite] Main and default autorole support

--- roleinvite/errors.py
+++ roleinvite/errors.py
@@ -1,29 +1,41 @@
 class Errors:
-    """Custom error handling used for the API"""
+    """
+    Custom error handling used for the cog and the API
+    """
 
-    class EmptyRolesList(Exception):
+    def __init__(self, exception):
+        pass
+
+    # errors used in the API
+    class EmptyRolesList:
         """
         The list of roles that needs to be linked to an invite is empty.
         """
         pass
 
-    class WrongInviteObject(Exception):
-        """
-        The invite object wasn't get using the good way. It can't get its number of uses. 
-        It need to be get using guild.invites() and discord.utils.get().
-        Not using Client.get_invite()
-        """
-        pass
 
-    class InviteNotFound(Exception):
+    class NotInvite:
+        """
+        The invite sent is not found as a discord.Invite object.
+        """
+
+    class InviteNotFound:
         """
         The invite sent isn't in the guild's invite list.
         """
         pass
 
-    class CannotGetInvites():
+    # errors used in the listener
+    class CannotGetInvites:
         """
         The bot isn't allowed to get the guild invites.
         Manage server permission is needed.
+        """
+        pass
+
+    class CannotAddRole:
+        """
+        The bot isn't allowed to give a role. 
+        The role hierarchy was modified or a 3rd party module added the role without check.
         """
         pass
