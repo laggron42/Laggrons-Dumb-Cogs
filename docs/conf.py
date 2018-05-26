23b6a7a670c2d01a8a74cc90d6a4a4abbf5f12b5
retke <travis@travis-ci.org>
2018-05-26 19:07:54 +0200

    [V3 Docs] Reformatted conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- General configuration ------------------------------------------------
@@ -182,4 +183,3 @@ intersphinx_mapping = {
     "discord": ("https://discordpy.readthedocs.io/en/rewrite/", None),
     "redbot": ("https://red-discordbot.readthedocs.io/en/v3-develop/", None),
 }
-
