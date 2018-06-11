fdec0a3b638ff6c8a299308ef9fd699ea6beb9bc
retke <travis@travis-ci.org>
2018-06-11 21:49:35 +0200

    [V3 Say] New cache system, allows multiple uploads at one

--- say/__init__.py
+++ say/__init__.py
@@ -1,5 +1,16 @@
+import pathlib
+import os
 from .say import Say
+from redbot.core.data_manager import cog_data_path
 
+def create_cache(path: pathlib.Path):
+    if not path.exists():
+        return
+    directories = [x for x in path.iterdir() if x.is_dir()]
+    if (path / 'cache') not in directories:
+        (path / 'cache').mkdir()
 
 def setup(bot):
-    bot.add_cog(Say(bot))
+    n = Say(bot)
+    create_cache(cog_data_path(n))
+    bot.add_cog(n)
