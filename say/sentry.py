0ac0ac03a305104a20b91cce905c8c2b68906328
retke <laggron42@gmail.com>
2018-08-03 21:08:09 +0200

    Add release and platform info

--- say/sentry.py
+++ say/sentry.py
@@ -8,21 +8,25 @@
 # This file is 95% from Cog-Creators for core Red
 
 import logging
+import platform
 
 from raven import Client
 from raven.handlers.logging import SentryHandler
+
 
 class Sentry:
     """
     Automatically send errors to the cog author
     """
 
-    def __init__(self, logger: logging.Logger):
+    def __init__(self, logger: logging.Logger, version):
         self.client = Client(
             dsn=(
                 "https://ff90c52be55a43b1914be6dd26ac7b57:dc1b6820fcfc4a149a2ff276a12b6ccf"
                 "@sentry.io/1253554"
-            )
+            ),
+            release=version,
+            environement=f"{platform.system()} ({platform.release()}"
         )
         self.handler = SentryHandler(self.client)
         self.logger = logger
