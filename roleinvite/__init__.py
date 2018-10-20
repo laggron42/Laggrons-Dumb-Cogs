e0387431b4673624e541059654f5ac3ce984c9ce
retke <laggron42@gmail.com>
2018-10-20 17:20:03 +0200

    [V3 RoleInvite] Ability to rename roleset to invteset

--- roleinvite/__init__.py
+++ roleinvite/__init__.py
@@ -1,6 +1,8 @@
 from .roleinvite import RoleInvite
 
 
-def setup(bot):
+async def setup(bot):
     n = RoleInvite(bot)
+    if await n.data.rename():
+        n.roleset.name = "inviteset"
     bot.add_cog(n)
