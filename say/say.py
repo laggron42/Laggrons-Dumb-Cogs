b5e7ab26fa045b37992af52bbcb8d521b2befe43
retke <laggron42@gmail.com>
2018-12-27 00:16:02 +0100

    [Say] Use redbot.core.utils.tunnel for downloading files

--- say/say.py
+++ say/say.py
@@ -10,6 +10,7 @@ from redbot.core import checks
 from redbot.core import Config
 from redbot.core.i18n import Translator, cog_i18n
 from redbot.core.data_manager import cog_data_path
+from redbot.core.utils.tunnel import Tunnel
 from redbot.core import commands
 
 from .sentry import Sentry
@@ -44,7 +45,7 @@ class Say(BaseCog):
         self.cache = cog_data_path(self) / "cache"
 
     __author__ = "retke (El Laggron)"
-    __version__ = "1.4.6"
+    __version__ = "1.4.7"
     __info__ = {
         "bot_version": "3.0.0b14",
         "description": (
@@ -75,73 +76,6 @@ class Say(BaseCog):
             The dictionnary that must appear on Sentry panel
         """
         self.sentry.client.extra_context(data)
-
-    async def download_files(self, message: discord.Message, author: discord.User = None):
-        """
-        Download all of the attachments linked to a message.
-
-        Arguments
-        ---------
-        message: discord.Message
-            The message to get the attachments.
-        author: discord.User
-            The user to send a message to in case of a downloading error.
-        
-        Returns
-        -------
-        files: list
-            Return a :py:class:`list` of :class:`discord.File`
-            downloaded in the cog's cache.
-        """
-
-        if message.attachments == []:
-            return []
-
-        total_bytes = 0
-        for attachment in message.attachments:
-            total_bytes += attachment.size
-        if total_bytes > 8000000:
-            await author.send(
-                _("The files got removed because they were too big. " "The limit for bots is 8mb.")
-            )
-            return []
-
-        exit_code = os.system(
-            f"wget --verbose --directory-prefix {str(self.cache)} "
-            + f"--output-file {str(self.cache)}/wget_log.txt "
-            + " ".join([x.url for x in message.attachments])
-        )
-        files = [discord.File(str(self.cache / x.filename)) for x in message.attachments]
-        if exit_code != 0:
-            # the file wasn't downloaded correctly
-            # let's tell the user what's wrong
-            error_message = _("An error occured while downloading the file.\n" "Error code ")
-            if exit_code == 1:
-                error_message += _("1: `wget` program not found, install it on your machine")
-            if exit_code == 3:
-                error_message += _("3: File I/O error (write permission)")
-            elif exit_code == 4:
-                error_message += _("4: Network failure")
-            elif exit_code == 5:
-                error_message += _("5: SSL verification failure")
-            elif exit_code == 7:
-                error_message += _("7: Protocol error")
-            elif exit_code == 8:
-                error_message += _("8: Server issued an error response")
-            elif exit_code == 2048:
-                error_message += _("2048: Image not found")
-            else:
-                error_message += _("unknown.")
-            # source: https://gist.github.com/cosimo/5747881
-
-            await author.send(error_message)
-            with open(str(self.cache / "wget_log.txt"), "r") as log_file:
-                log.warning(
-                    f"Exception in downloading files. Exit code {exit_code}.\n"
-                    f"Full output: {log_file.read()}"
-                )
-            return []
-        return files
 
     async def say(self, ctx, text, files):
 
@@ -185,7 +119,7 @@ class Say(BaseCog):
                         _("I am not allowed to send messages in ") + channel.mention,
                         delete_after=2,
                     )
-                except discord.errors.Forbidden as e:
+                except discord.errors.Forbidden:
                     await author.send(
                         _("I am not allowed to send messages in ") + channel.mention,
                         delete_after=15,
@@ -196,7 +130,7 @@ class Say(BaseCog):
                     await ctx.send(
                         _("I am not allowed to upload files in ") + channel.mention, delete_after=2
                     )
-                except discord.errors.Forbidden as e:
+                except discord.errors.Forbidden:
                     await author.send(
                         _("I am not allowed to upload files in ") + channel.mention,
                         delete_after=15,
@@ -222,7 +156,7 @@ class Say(BaseCog):
         - `!say owo I have a file` (a file is attached to the command message)
         """
 
-        files = await self.download_files(ctx.message, ctx.author)
+        files = await Tunnel.files_from_attatch(ctx.message)
         await self.say(ctx, text, files)
 
     @commands.command(name="sayd", aliases=["sd"])
@@ -230,20 +164,20 @@ class Say(BaseCog):
     async def _saydelete(self, ctx, *, text: str = ""):
         """
         Same as say command, except it deletes your message.
-        
+
         If the message wasn't removed, then I don't have enough permissions.
         """
 
         # download the files BEFORE deleting the message
         author = ctx.author
-        files = await self.download_files(ctx.message, author)
+        files = await Tunnel.files_from_attatch(ctx.message)
 
         try:
             await ctx.message.delete()
         except discord.errors.Forbidden:
             try:
                 await ctx.send(_("Not enough permissions to delete messages."), delete_after=2)
-            except discord.errors.Forbidden as e:
+            except discord.errors.Forbidden:
                 await author.send(_("Not enough permissions to delete messages."), delete_after=15)
 
         await self.say(ctx, text, files)
@@ -258,7 +192,8 @@ class Say(BaseCog):
             if isinstance(ctx.channel, discord.DMChannel):
                 await ctx.send(
                     _(
-                        "You need to give a channel to enable this in DM. You can give the channel ID too."
+                        "You need to give a channel to enable this in DM. You can "
+                        "give the channel ID too."
                     )
                 )
                 return
@@ -294,16 +229,8 @@ class Say(BaseCog):
                 return
 
             if message.author == u and isinstance(message.channel, discord.DMChannel):
-                if message.attachments != []:
-                    os.system("wget " + message.attachments[0].url)
-                    await channel.send(
-                        message.content, file=discord.File(message.attachments[0].filename)
-                    )
-                    os.remove(message.attachments[0].filename)
-
-                else:
-                    await channel.send(message.content)
-
+                files = await Tunnel.files_from_attatch(message)
+                await channel.send(message.content, files=files)
             elif (
                 message.channel != channel
                 or message.author == channel.guild.me
@@ -344,7 +271,8 @@ class Say(BaseCog):
 
             await ctx.send(
                 _(
-                    "You're about to {} error logging. Are you sure you want to do this? Type `yes` to confirm."
+                    "You're about to {} error logging. Are you sure you want "
+                    "to do this? Type `yes` to confirm."
                 ).format(not current_status)
             )
             try:
