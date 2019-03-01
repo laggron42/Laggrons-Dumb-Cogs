24d552da02da8f9412f1d24116567db39254d7d6
retke <laggron42@gmail.com>
2019-03-01 19:28:37 +0100

    [WarnSystem] Tell the user that the cog conflicts with Warnings
    This is told in the desciption and install_msg fields of info.json

--- warnsystem/warnsystem.py
+++ warnsystem/warnsystem.py
@@ -164,14 +164,16 @@ class WarnSystem(BaseCog):
             "An alternative to the core moderation cog, similar to Dyno.\n"
             "The cog allows you to take actions against member and keep track with "
             "a new modlog system. It also sends a DM to the warned members.\n\n"
-            "This is the rewrite of the V2 BetterMod cog."
+            "This is the rewrite of the V2 BetterMod cog. **Note that this cog "
+            "conflicts with Warnings which must be unloaded.**"
         ),
         "hidden": False,
         "install_msg": (
             "Thank you for installing the warnsystem cog. Please check the wiki "
             "for all informations about the cog.\n"
             "https://laggrons-dumb-cogs.readthedocs.io/warnsystem.html\n\n"
-            "Type `[p]help WarnSystem` for a quick overview of the commands."
+            "Type `[p]help WarnSystem` for a quick overview of the commands\n"
+            "**This cog conflicts with Warnings which must be unloaded.**"
         ),
         "required_cogs": [],
         "requirements": [],
