631c137073073933b33274ae45aaa94d2f9a60ac
retke <laggron42@gmail.com>
2019-06-03 13:01:11 +0200

    [Docs] Fix errors related to discord.py release

--- docs/conf.py
+++ docs/conf.py
@@ -184,7 +184,7 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3.6", None),
-    "discord": ("https://discordpy.readthedocs.io/en/rewrite/", None),
+    "discord": ("https://discordpy.readthedocs.io/en/latest/", None),
     "redbot": ("https://red-discordbot.readthedocs.io/en/v3-develop/", None),
 }
 
