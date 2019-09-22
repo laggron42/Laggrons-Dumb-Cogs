03f900809fafae812a3f260743e391e43c3d56f7
retke <laggron42@gmail.com>
2019-09-22 19:37:06 +0200

    Allow mods to edit their own warnings

--- warnsystem/warnsystem.py
+++ warnsystem/warnsystem.py
@@ -725,7 +725,7 @@ class WarnSystem(SettingsMixin, BaseCog, metaclass=CompositeMetaClass):
             embeds.append(embed)
 
         controls = {"⬅": menus.prev_page, "❌": menus.close_menu, "➡": menus.next_page}
-        if await mod.is_mod_or_superior(self.bot, ctx.author) and user != ctx.author:
+        if await mod.is_mod_or_superior(self.bot, ctx.author):
             controls.update({"✏": self._edit_case, "🗑": self._delete_case})
 
         await menus.menu(
