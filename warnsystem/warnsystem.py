fce33218b9943b6b6d98b6da6255cc8d3d123e3b
retke <laggron42@gmail.com>
2019-12-07 12:27:25 +0100

    Bump version to 1.3.0

--- warnsystem/warnsystem.py
+++ warnsystem/warnsystem.py
@@ -147,7 +147,7 @@ class WarnSystem(SettingsMixin, BaseCog, metaclass=CompositeMetaClass):
         self.task = bot.loop.create_task(self.api._loop_task())
         self._init_logger()
 
-    __version__ = "1.2.5"
+    __version__ = "1.3.0"
     __author__ = ["retke (El Laggron)"]
 
     def _init_logger(self):
