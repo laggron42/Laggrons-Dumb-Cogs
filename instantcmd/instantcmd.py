1e7c9e70fd142e2160e87adf4f417bc3cc72e516
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-03-08 19:54:19 +0100

    Update instantcmd.py

--- instantcmd/instantcmd.py
+++ instantcmd/instantcmd.py
@@ -189,7 +189,7 @@ class InstantCommands(BaseCog):
             "Would you like to replace it?"
         )
         pred = ReactionPredicate.yes_or_no(msg, ctx.author)
-        start_adding_reactions(msg, ReactionPredicate.YES_OR_NO_EMOJIS, loop=ctx.bot.loop)
+        start_adding_reactions(msg, ReactionPredicate.YES_OR_NO_EMOJIS)
         try:
             await self.bot.wait_for("reaction_add", check=pred, timeout=30)
         except asyncio.TimeoutError:
