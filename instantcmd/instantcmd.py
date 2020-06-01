5f83bd171eb26ecabf7642feb20b0b38be2e1962
El Laggron <laggron42@gmail.com>
2020-06-01 21:37:51 +0200

    Bump to next minor version

--- instantcmd/instantcmd.py
+++ instantcmd/instantcmd.py
@@ -75,7 +75,7 @@ class InstantCommands(BaseCog):
         self._init_logger()
 
     __author__ = ["retke (El Laggron)"]
-    __version__ = "1.1.1"
+    __version__ = "1.1.2"
 
     def _init_logger(self):
         log_format = logging.Formatter(
