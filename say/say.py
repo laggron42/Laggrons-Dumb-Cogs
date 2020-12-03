bf726eee178641d42aaadea1e420fabfdef7b191
retke <laggron42@gmail.com>
2020-12-03 11:04:04 +0100

    Forgot to bump the version number :NotLikeThis:

--- say/say.py
+++ say/say.py
@@ -36,7 +36,7 @@ class Say(BaseCog):
         self.interaction = []
 
     __author__ = ["retke (El Laggron)"]
-    __version__ = "1.4.14"
+    __version__ = "1.5.0"
 
     async def say(
         self,
