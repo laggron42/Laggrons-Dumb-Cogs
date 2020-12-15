298cba0a8732d287ee565d49757ac656c1a53aae
retke <laggron42@gmail.com>
2020-12-15 12:04:58 +0100

    [WarnSystem] Safe regex processing
    Thanks TrustyJAID for his amazing stuff
    Resolves #125
    
    Squashed commit of the following:
    
    commit f648611ee6afe4f2302b63ef169b1b2f525d700f
    Author: retke <laggron42@gmail.com>
    Date:   Tue Dec 15 12:01:54 2020 +0100
    
        Make it work
    
    commit 287c1d1415af70d962d158b4c3ee2d918e8d597c
    Author: retke <laggron42@gmail.com>
    Date:   Mon Dec 14 11:21:06 2020 +0100
    
        Safe regex processing for automod

--- warnsystem/warnsystem.py
+++ warnsystem/warnsystem.py
@@ -224,7 +224,7 @@ class WarnSystem(SettingsMixin, AutomodMixin, BaseCog, metaclass=CompositeMetaCl
 
         self.task: asyncio.Task
 
-    __version__ = "1.3.10"
+    __version__ = "1.3.11"
     __author__ = ["retke (El Laggron)"]
 
     # helpers
