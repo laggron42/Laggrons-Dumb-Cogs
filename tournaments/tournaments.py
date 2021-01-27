080838aa10b890d356b9a3ae6c75ae19d2ecd457
retke <laggron42@gmail.com>
2021-01-27 13:16:04 +0100

    Version bump

--- tournaments/tournaments.py
+++ tournaments/tournaments.py
@@ -131,7 +131,7 @@ class Tournaments(
         except Exception as e:
             log.error("Couldn't load dev env values.", exc_info=e)
 
-    __version__ = "1.0.0"
+    __version__ = "1.0.1"
     __author__ = ["retke (El Laggron)", "Wonderfall", "Xyleff"]
 
     @commands.command(hidden=True)
