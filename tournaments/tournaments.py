88c5ae9e46b916f73a0db132b1119723ac65eb5e
retke <laggron42@gmail.com>
2021-03-09 09:31:01 +0100

    That was useless, reverting...

--- tournaments/tournaments.py
+++ tournaments/tournaments.py
@@ -42,19 +42,6 @@ class TournamentsConfig(Config):
     Just a shortcut for custom groups.
     """
 
-    def __init__(self, cog_name, unique_identifier, driver, force_registration, defaults):
-        super().__init__(
-            cog_name,
-            unique_identifier,
-            driver,
-            force_registration=force_registration,
-            defaults=defaults,
-        )
-
-    @classmethod
-    def get_conf(cls, *args, **kwargs):
-        return super().get_conf(*args, **kwargs)
-
     def settings(self, *args, **kwargs) -> Group:
         return self.custom("SETTINGS", *args, *kwargs)
 
