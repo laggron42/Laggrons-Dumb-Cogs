5c6a386be70d245a86b821c2d744586e79c2395a
retke <laggron42@gmail.com>
2021-05-17 15:01:55 +0200

    [Tournaments] Check for aiodns presence on load

--- tournaments/__init__.py
+++ tournaments/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import aiohttp
 import importlib.util
 
 from redbot.core.errors import CogLoadError
@@ -126,9 +127,38 @@ async def restore_tournaments(bot, cog):
     await cog.restore_tournaments()
 
 
+def check_for_aiodns():
+    """
+    The achallonge library has special behaviour with resolvers
+    It checks on load if aiodns is available, to determine which resolver to use.
+
+    However, aiohttp will return instances of AsyncResolver only if the "aiodns" lib is
+    available on load, else it will fail.
+
+    Basically there's a problem if aiodns is installed with downloader, since aiohttp was
+    initialized without aiodns, and achallonge believes it was, as long as the lib is importable.
+    """
+    try:
+        import aiodns
+
+        aiohttp.AsyncResolver()
+    except ImportError:
+        pass
+    except RuntimeError as e:
+        raise CogLoadError(
+            "There is a problem with the `aiodns` library which cannot properly load. "
+            "Please do one of the following:\n"
+            "- Properly install `aiodns` in your environment (use `python3.8 -m pip install "
+            "aiodns`, __the `[p]pipinstall` command will not work__), then restart the bot\n"
+            "- Fully uninstall the `aiodns` library (it can be hidden in "
+            "`redbot/cogs/Downloader/lib/`)"
+        ) from e
+
+
 async def setup(bot):
     init_logger(log, "Tournaments")
     n = Tournaments(bot)
+    check_for_aiodns()
     try:
         n.data.settings
     except AttributeError:
