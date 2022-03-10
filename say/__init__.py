42506f9a04553ff32b493dbc8046a560a05cf8de
retke <laggron42@gmail.com>
2022-03-10 18:03:42 +0100

    [Say] Add /say slash command

--- say/__init__.py
+++ say/__init__.py
@@ -1,9 +1,16 @@
+import asyncio
 import logging
 import importlib.util
+
 from .say import Say
 
+from typing import TYPE_CHECKING
+from discord import app_commands
 from redbot.core.errors import CogLoadError
 from laggron_utils import init_logger
+
+if TYPE_CHECKING:
+    from redbot.core.bot import Red
 
 if not importlib.util.find_spec("laggron_utils"):
     raise CogLoadError(
@@ -16,8 +23,29 @@ if not importlib.util.find_spec("laggron_utils"):
 log = logging.getLogger("red.laggron.say")
 
 
-async def setup(bot):
-    init_logger(log, Say.__class__.__name__)
+async def setup(bot: "Red"):
+    init_logger(log, "Say")
+    try:
+        if not hasattr(bot, "tree"):
+            bot.tree = app_commands.CommandTree(bot)
+    except AttributeError:
+        raise CogLoadError("This cog requires the latest discord.py 2.0.0a.") from None
     n = Say(bot)
     bot.add_cog(n)
+    asyncio.create_task(_setup(bot))
     log.debug("Cog successfully loaded on the instance.")
+
+
+async def _setup(bot: "Red"):
+    if bot.user:
+        assert isinstance(bot.tree, app_commands.CommandTree)
+        log.debug("Added slash command /say, syncing...")
+        await bot.tree.sync(guild=None)
+        log.debug("Slash commands now synced...")
+
+
+def teardown(bot: "Red"):
+    if bot.user:
+        assert isinstance(bot.tree, app_commands.CommandTree)
+        # delay the slash removal a bit in case this is a reload
+        asyncio.get_event_loop().call_later(2, asyncio.create_task, bot.tree.sync(guild=None))
