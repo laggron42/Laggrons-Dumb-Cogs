5f3bba0d4d8e5eefc4428b21bf316fa0c0e3fbb2
EternalllZM <43221983+EternalllZM@users.noreply.github.com>
2023-07-11 17:11:00 -0500

    Fix banned member warnings display

--- warnsystem/warnsystem.py
+++ warnsystem/warnsystem.py
@@ -756,7 +756,7 @@ class WarnSystem(SettingsMixin, AutomodMixin, commands.Cog, metaclass=CompositeM
                 msg.append(f"{warning_str(i, total_warns > 1)}: {total_warns}")
         warn_field = "\n".join(msg) if len(msg) > 1 else msg[0]
         embed = discord.Embed(description=_("User modlog summary."))
-        embed.set_author(name=f"{user} | {user.id}", icon_url=user.avatar.url)
+        embed.set_author(name=f"{user} | {user.id}", icon_url=user.display_avatar)
         embed.add_field(
             name=_("Total number of warnings: ") + str(len(cases)), value=warn_field, inline=False
         )
